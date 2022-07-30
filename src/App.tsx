f9b2a7b4e18569b72f3a986bdf03213e31853dbe
Felipe Ribeiro <fefeucn_cm@hotmail.com>
2022-07-30 19:53:27 -0300

    Finalização

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import PageLayout from './pages/PageLayout';
+import Home from './pages/Home';
+import Sortition from './pages/Sortition';
 
 function App() {
   return (
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={<PageLayout />}/>
+          <Route path='/' element={<Home />}/>
+          <Route path='/sorteio' element={<Sortition />}/>
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
