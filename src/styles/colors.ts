5a2279a50920d86c5db656820cfb74a1a10d784f
Felipe Ribeiro <fefeucn_cm@hotmail.com>
2022-07-30 19:48:24 -0300

    Camada de cores do sorteio

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  bg: {
+    primary: "#4B69FD",
+    secondary: {
+      coral: '#FE652B',
+      black: '#000000'
+    },
+    terciary: '#FFF9EB',
+    warning: '#f8d7da'
+  },
+  text: {
+    primary: '#FFFFFF',
+    secondary: '#000000',
+    warning: '#842029'
+  }
+}
